14bce1322f99bdb5d5c0d7c4b2ecb077dbc135c6
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-04-16 14:15:36 -0400

    feat(Impulse-5): setup backend

---
+++ impulse-backend/src/main/java/com/impulse/backend/ImpulseBackendApplication.java
@@ -0,0 +1,15 @@
+package com.impulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulseBackendApplication.class, args);
+	}
+
+}
+
+
---
+++ impulse-backend/src/test/java/com/impulse/backend/ImpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
