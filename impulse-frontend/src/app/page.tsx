bb6d601b38843836b61ea705728f1d3fbeb97f7c
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-05-15 12:56:47 -0400

    feat(Impulse-25): created landing page

--- impulse-frontend/src/app/page.tsx
+++ impulse-frontend/src/app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="h-fit my-auto flex items-center gap-24 flex-col lg:flex-row p-4">
+      <Image
+        src={"/impulse-logo.svg"}
+        width={350}
+        height={350}
+        alt="impulse logo"
+      />
+      <div className="flex flex-col items-center gap-4">
+        <h1>Impulse</h1>
+        <h2>Track Prices. Shop Smarter.</h2>
+        <Button size="cta" className="mt-16 w-full font-bold" asChild>
+          <Link href="/login">Get Started</Link>
+        </Button>
+      </div>
     </div>
   );
 }
