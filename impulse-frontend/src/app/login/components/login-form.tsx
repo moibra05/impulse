c81e946a94a53550fa03e6da221a31a6dd856dea
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-05-15 20:59:35 -0400

    feat: craeted login page

---
+++ impulse-frontend/src/app/login/components/login-form.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+const schema = z.object({
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+  password: z.string().trim().min(1, { message: "Password is required" }),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-8 w-full max-w-[500px] p-8 m-6 rounded bg-secondary"
+      >
+        <h2 className="">Welcome Back.</h2>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@email.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="&#183; &#183; &#183; &#183; &#183; &#183; &#183; &#183;"
+                  type="password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+              <Button variant="link" size="link" className="ml-auto">
+                <Link href="/forgot-password">Forgot your password?</Link>
+              </Button>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" size="submit">
+          Submit
+        </Button>
+        <div className="flex items-center">
+          <div className="flex-1 h-[2px] bg-muted mr-3"></div>
+          Or
+          <div className="flex-1 h-[2px] bg-muted ml-3"></div>
+        </div>
+        <div className="flex items-center justify-center">
+          <p>Don&apos;t have an account yet?</p>
+          <Button variant="link" size="link" className="ml-2">
+            <Link href="sign-up">Sign up</Link>
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
--- impulse-frontend/src/app/login/page.tsx
+++ impulse-frontend/src/app/login/page.tsx
@@ -1,26 +1,9 @@
-"use client";
-
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-const schema = z.object({
-  email: z
-    .string({ required_error: "Email is required" })
-    .email({ message: "Invalid email address" }),
-  password: z.string({ required_error: "Password is required" }),
-});
+import LoginForm from "./components/login-form";
 
 export default function Login() {
-  const form = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      email: "",
-      password: "",
-    }
-  })
-
-  function onSubmit(values: z.infer<typeof schema>) {
-    console.log(values);
-  }
+  return (
+    <main className="flex items-center justify-center w-full">
+      <LoginForm />
+    </main>
+  );
 }
