5f428aba7b421f71962cf8a6438e9c7340090079
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-07-27 20:04:23 -0400

    fix: made get started button send to dashboard instead of login if user logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { createClient } from "../../utils/supabase/client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [redirectLink, setRedirectLink] = useState("/sign-up");
+
+  useEffect(() => {
+    async function checkAuth() {
+      const supabase = await createClient();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user !== null) {
+        setRedirectLink("/dashboard");
+      }
+    }
+
+    checkAuth();
+  }, []);
+
   return (
     <main className="flex items-center gap-8 lg:gap-24 flex-col lg:flex-row p-4 justify-center">
       <Image
@@ -16,7 +37,7 @@ export default function Home() {
         <h1>Impulse</h1>
         <h2>Track Prices. Shop Smarter.</h2>
         <Button size="cta" className="mt-8 lg:mt-16 w-full font-bold" asChild>
-          <Link href="/sign-up">Get Started</Link>
+          <Link href={redirectLink}>Get Started</Link>
         </Button>
       </div>
     </main>
